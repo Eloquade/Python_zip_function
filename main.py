14bcff20f9fc28acdc61d03d73197378dd41fbf3
lawrence <lpjm08@gmail.com>
2021-06-23 18:36:43 +0800

    Added Unzipped

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 list1 = [1, 2, 3, 4, 5, 6]
 list2 = ['one', 'two', 'three', 'four', 'five', 'six']
 
-zipped = list(zip(list1,list2))
+zipped = list(zip(list1, list2))
 print(zipped)
+
+unzipped = list(zip(*zipped))
+print(unzipped)
